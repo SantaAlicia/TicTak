b81f95a8d5f8b61a817d2b5da760c4857601b9b7
liudmila vladimirova <wlucy@yandex.ru>
2019-07-27 17:43:25 +0200

    Initial Commit

---
+++ TicTak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTak
+//
+//  Created by liudmila vladimirova on 27/07/2019.
+//  Copyright © 2019 SantaAlicia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
