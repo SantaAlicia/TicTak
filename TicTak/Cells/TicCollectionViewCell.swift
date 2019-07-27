d1dd9114847ec427835fd029d6151f1daf254f4c
liudmila vladimirova <wlucy@yandex.ru>
2019-07-27 20:18:35 +0200

    CollectionView was added

---
+++ TicTak/Cells/TicCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TicCollectionViewCell.swift
+//  TicTak
+//
+//  Created by liudmila vladimirova on 27/07/2019.
+//  Copyright © 2019 SantaAlicia. All rights reserved.
+//
+
+import UIKit
+
+class TicCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    
+//    override func prepareForReuse() {
+//        super.prepareForReuse()
+//
+//        self.imgView.backgroundColor = UIColor.lightGray
+//    }
+}
+
--- TicTak/ViewController.swift
+++ TicTak/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TicTak
 //
-//  Created by liudmila vladimirova on 27/07/2019.
+//  Created by SantaAlicia on 27/07/2019.
 //  Copyright © 2019 SantaAlicia. All rights reserved.
 //
 
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    let reuseIdentifier = "ticTakCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//
+//        if let flowLayout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+//            flowLayout.itemSize = CGSize(width: self.collectionView.bounds.width, height: 120)
+//        }
+//    }
+}
 
+extension ViewController: UICollectionViewDataSource {
 
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TicCollectionViewCell
+        cell.imgView.backgroundColor = UIColor.lightGray
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item + 1)
+    }
 }
 
