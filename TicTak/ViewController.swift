52da69fe0e53725384aa50f1b396af39eff5db4f
liudmila vladimirova <wlucy@yandex.ru>
2020-01-28 01:22:22 +0100

    created Imbeded View - CollectionView

--- TicTak/ViewController.swift
+++ TicTak/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var gameOverInfo: UILabel!
     @IBOutlet weak var gameInfo: UILabel!
     @IBOutlet weak var playWithComputerSwitch: UISwitch!
     
-    let reuseIdentifier = "ticTakCell"
-    
+    private var embeddedViewController: CollectionViewController!
     private let sectionInsets = UIEdgeInsets(top: 0.0,
     left: 0.0,
     bottom: 0.0,
@@ -32,10 +30,17 @@ class ViewController: UIViewController {
        preparationViewsAndContols()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? CollectionViewController,
+                    segue.identifier == "collectionViewSegue" {
+            self.embeddedViewController = vc
+        }
+    }
+    
     func tuningView () {
         startButton.layer.cornerRadius = DesignConstants.cornerRadius
         startButton.clipsToBounds = true
-        collectionView.backgroundColor = UIColor(patternImage: UIImage(named: "notebookBackground")!)
+        embeddedViewController.collectionView.backgroundColor = UIColor(patternImage: UIImage(named: "notebookBackground")!)
         view.backgroundColor = UIColor(patternImage: UIImage(named: "woodBackground")!)
         playWithComputerSwitch.onTintColor = .black
         playWithComputerSwitch.tintColor = .black
@@ -52,127 +57,33 @@ class ViewController: UIViewController {
 //    }
 }
 
-extension ViewController: UICollectionViewDataSource {
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return GameConstants.gameDimension
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TicTacCollectionViewCell
-
-        collectionCell.layer.borderColor = UIColor.lightGray.cgColor
-        
-        collectionCell.isUserInteractionEnabled = true
-        let isWinCell = game.isCellInsideWinCombination(index: indexPath.row)
-
-        collectionCell.fillCell(type : game.gameBoard.cellAtIndex(indexPath.row).type, isWinCell: isWinCell )
-        return collectionCell
-    }
-}
-
-extension ViewController: UICollectionViewDelegate {
-    
-   func playerMakesOneMove(_ i : Int) {
-                if (game.playerMakesMoveAtIndex(i)) {
-                    
-                    if ((playWithComputerSwitch.isOn) && (game.currentPlayer == Player.zero)) {
-                        gameTimer?.invalidate()
-                        gameTimer = nil
-                    }
-                    collectionView.reloadData()
-                    updateInfoLabels()
-                    updateEnabledControls()
-                }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-                playerMakesOneMove(indexPath.row)
-                if (game.state == GameState.isOver) {
-                    gameTimer?.invalidate()
-                    gameTimer = nil
-                    collectionView.isUserInteractionEnabled = false
-                    return
-                }
-                if ((playWithComputerSwitch.isOn) && (game.currentPlayer == Player.zero)) {
-                    
-                    guard let ramdomIndexFromEmptyCell = game.gameBoard.findOneEmptyCell() else {
-                        return
-                    }
-
-                    gameTimer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true, block: { timer in
-                        self.playerMakesOneMove(ramdomIndexFromEmptyCell)
-                    })
-                }
-            }
-}
-
-extension ViewController : UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView,
-//                        layout collectionViewLayout: UICollectionViewLayout,
-//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-//      //2
-//      let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
-//      let availableWidth = view.frame.width - paddingSpace
-//      let widthPerItem = availableWidth / itemsPerRow
-//
-//      return CGSize(width: widthPerItem, height: widthPerItem)
-//    }
-    
-//    //3
-//    func collectionView(_ collectionView: UICollectionView,
-//                        layout collectionViewLayout: UICollectionViewLayout,
-//                        insetForSectionAt section: Int) -> UIEdgeInsets {
-//      return sectionInsets
-//    }
-//
-//    // 4
-//    func collectionView(_ collectionView: UICollectionView,
-//                        layout collectionViewLayout: UICollectionViewLayout,
-//                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//      return sectionInsets.left
-//    }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        let nbCol = 3
-        
-
-        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-        let totalSpace = flowLayout.sectionInset.left
-            + flowLayout.sectionInset.right
-            + (flowLayout.minimumInteritemSpacing * CGFloat(nbCol - 1))
-        let size = Int((collectionView.bounds.width - totalSpace) / CGFloat(nbCol))
-        
-//        print("***")
-//             print(collectionView.bounds.size.width)
-//             print(collectionView.bounds.size.height)
-        
-        let w = collectionView.bounds.size.width
-        collectionView.bounds.size = CGSize(width: w, height: w)
-        return CGSize(width: size, height: size)
-    }
-}
 
 extension ViewController {
     
-private func startNewGame() {
-        game.startNewGame()
-        preparationViewsAndContols()
-        collectionView.isUserInteractionEnabled = true
-}
+    private func startNewGame() {
+            game.startNewGame()
+            preparationViewsAndContols()
+            embeddedViewController.collectionView.isUserInteractionEnabled = true
+    }
     
-private func preparationViewsAndContols() {
-        collectionView.reloadData()
-        updateInfoLabels()
-        updateEnabledControls()
-}
+    private func preparationViewsAndContols() {
+            embeddedViewController.collectionView.reloadData()
+            updateInfoLabels()
+            updateEnabledControls()
+    }
+        
+    func playerMakesOneMove(_ i : Int) {
+                    if (game.playerMakesMoveAtIndex(i)) {
+                        
+                        if ((playWithComputerSwitch.isOn) && (game.currentPlayer == Player.zero)) {
+                            gameTimer?.invalidate()
+                            gameTimer = nil
+                        }
+                        embeddedViewController.collectionView.reloadData()
+                        updateInfoLabels()
+                        updateEnabledControls()
+                    }
+        }
     
 private func updateInfoLabels () {
     switch game.state {
