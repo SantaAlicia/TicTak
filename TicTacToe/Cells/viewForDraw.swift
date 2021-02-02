9d3b560a9077c10f701562d1ce5dbe6096323a07
liudmila vladimirova <wlucy@yandex.ru>
2021-02-02 18:10:35 +0100

    Improvements - changing draw color, rounded edges, size of circle, bigger font in leftMenu

--- TicTacToe/Cells/viewForDraw.swift
+++ TicTacToe/Cells/viewForDraw.swift
@@ -30,12 +30,21 @@ class ColorShema {
     static var colorShemaType : ColorShemaType = ColorShemaType.whiteGray {
         didSet {
             if colorShemaType == ColorShemaType.whiteGray  {
+                let whiteImage = UIImage(named: "chalk")!
+                white = (UIColor(patternImage: whiteImage)).cgColor
+                
                 ColorShema.crossColor = white
                 ColorShema.circleColor = white
                 ColorShema.borderColor = borderWG
                 ColorShema.backgroungColor = backgroundWG
             }
             if colorShemaType == ColorShemaType.blackRed  {
+                let redImage = UIImage(named: "redChalk")!
+                red = (UIColor(patternImage: redImage)).cgColor
+                
+                let blackImage = UIImage(named: "blackChalk")!
+                black = (UIColor(patternImage: blackImage)).cgColor
+                
                 ColorShema.crossColor = red
                 ColorShema.circleColor = black
                 ColorShema.borderColor = borderBR
@@ -48,9 +57,9 @@ class ColorShema {
     static var borderColor : CGColor!
     static var backgroungColor : UIColor!
 
-    private static let white: CGColor = UIColor.white.cgColor
-    private static let black : CGColor  = UIColor.black.cgColor
-    private static let red : CGColor = UIColor.red.cgColor
+    private static var white: CGColor = UIColor.white.cgColor
+    private static var black : CGColor  = UIColor.black.cgColor
+    private static var red : CGColor = UIColor.red.cgColor
     private static let borderWG : CGColor = UIColor.blue.cgColor
     private static let borderBR : CGColor = UIColor.lightGray.cgColor
     private static let backgroundWG : UIColor = UIColor.darkGray
@@ -63,7 +72,7 @@ class Figure {
     
     func createFigure(view : UIView, cellType:CellType, isCurrent: Bool) {
         view.backgroundColor = ColorShema.backgroungColor
-       
+        
        if (cellType == .empty) { return }
        if (cellType == .cross) {
            figureView =  CrossView(frame: view.frame, drawNow: isCurrent)
@@ -96,7 +105,7 @@ class CircleView: FigureView {
     init(frame : CGRect, drawNow : Bool){
         super.init(frame : frame)
         self.backgroundColor = UIColor.clear
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: (frame.size.width - 20)/2, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2.0), clockwise: true)
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: (frame.size.width - 28)/2, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2.0), clockwise: true)
         
         figureLayer = CAShapeLayer()
         figureLayer.path = circlePath.cgPath
@@ -118,10 +127,10 @@ class CrossView: FigureView {
         self.backgroundColor = UIColor.clear
         
         let crossPath = UIBezierPath()
-        let delta = CGFloat(8)
+        let delta = CGFloat(15)
         crossPath.move(to: CGPoint(x: 0+delta, y: 0+delta))
         crossPath.addLine(to: CGPoint(x: frame.size.width-delta, y: frame.size.height-delta))
-        crossPath.close()
+        //crossPath.close()
         crossPath.move(to: CGPoint(x: frame.size.width-delta, y: 0+delta))
         crossPath.addLine(to: CGPoint(x: 0+delta, y: frame.size.height-delta))
         
@@ -130,6 +139,7 @@ class CrossView: FigureView {
         figureLayer.fillColor = UIColor.clear.cgColor
         figureLayer.strokeColor = ColorShema.crossColor
         figureLayer.lineWidth = 11.0
+        figureLayer.lineCap = CAShapeLayerLineCap.round
     
         figureLayer.strokeEnd = (drawNow) ?  0 : 1
         layer.addSublayer(figureLayer)
